75324029995ec767111d7b6aa400df0575a53df3
Hettwein <heiko.ettwein@gmx.de>
2016-05-19 19:47:45 +0200

    with first provisional gui

--- Tumbleweed.py
+++ Tumbleweed.py
@@ -8,18 +8,28 @@ Created on Tue May 17 15:15:36 2016
 import socket
 import threading
 import sys
+from tkinter import *
 
-#PORT_LISTENING = sys.argv[1]
 
 buddy_list = {}
+gui_buddy_list = 1
+chat_box = 1
+nickname = ""
+PORT_LISTENING = 50000
+listener_stop= threading.Event()
+client_listener_stop= threading.Event()
+input_stop= threading.Event()
 
-nickname = input('Please enter nickname: ')
-print('\nWelcome ', nickname, '!\n')
-if len(sys.argv) > 1:
-    PORT_LISTENING = int(sys.argv[1])
-    print("listening on " + sys.argv[1])
-else:
-    PORT_LISTENING = 50001
+
+def main(argv):
+    global nickname
+    global PORT_LISTENING
+    #nickname = input('Please enter nickname: ')
+    #print('\nWelcome ', nickname, '!\n')
+    if len(sys.argv) > 1:
+        PORT_LISTENING = int(sys.argv[1])
+    #print("listening on " + str(PORT_LISTENING))
+    gui()
 
 
 def input_processing():
@@ -46,7 +56,7 @@ def input_processing():
             break
 
 def port_listener():
-    print("start listening")    
+    print("start listening on " + str(PORT_LISTENING))    
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('127.0.0.1', PORT_LISTENING))
     sock.listen(10)
@@ -78,12 +88,16 @@ def client_listener(addr, connection):
             if data.split(';')[0] == 'Q':
                 buddy_list[buddy_name].close()
                 buddy_list.pop(buddy_name)
+                update()
                 print(buddy_name + " has disconnected.\n")
+                newMessage(buddy_name + ' has disconnected.')
                 break
             elif data.split(';')[0] == 'C':
                 print(buddy_name + ': ' + data.split(';')[1])
+                newMessage(buddy_name + ': ' + data.split(';')[1])
             elif data.split(';')[0] == 'G':
                 print('[All]' + buddy_name + ': ' + data.split(';')[1])
+                newMessage(buddy_name + ': ' + data.split(';')[1])
             elif data.split(';')[0] == 'D':
                 connect(data.split(';')[1], data.split(';')[2], 1)
             elif data.split(';')[0] =='N':
@@ -93,28 +107,30 @@ def client_listener(addr, connection):
                     if int(data.split(';')[3]) == 2:
                         for buddy in buddy_list:
                             buddy_list[buddy].send(('D;' + addr[0] + ';' + data.split(';')[2]).encode('utf-8'))
-                    connect(addr[0], data.split(';')[2])       
+                    connect(addr[0], data.split(';')[2])
+                    newMessage(buddy_name + ' has connected.')
 
                 
         except:
             print("Unexpected error:", sys.exc_info()[0])
-            disconnect()
+            #disconnect()
             buddy_list[buddy_name].shutdown(socket.SHUT_RDWR)
             buddy_list[buddy_name].close()
-            print("\nDisconnected.\n")
+            buddy_list.pop(buddy_name)
+            update()
+            newMessage(buddy_name + ' has disconnected.')
             break
     print("Connection to" + str(addr) + " closed.\n")
 
 def connect(addr, port, new=0):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1)
-    print(port)
-    print(addr)
     if sock.connect_ex((addr, int(port))) == 0:
         try:
             sock.send(('N;' + nickname + ';' + str(PORT_LISTENING) + ';' + str(new)).encode('utf-8'))
             buddy = sock.recv(1024).decode('utf-8')
             buddy_list[buddy] = sock
+            update()
         except:
             pass
 
@@ -128,10 +144,12 @@ def discover(addr):
                 sock.send(('N;' + nickname + ';' + str(PORT_LISTENING) + ';2').encode('utf-8'))
                 buddy = sock.recv(1024).decode('utf-8')
                 buddy_list[buddy] = sock
+                update()
                 break
             except:
                 pass
         port += 1
+    newMessage('You have connected to ' + addr + '.')
     
 
 def scan():
@@ -145,11 +163,13 @@ def buddys():
 
 
 def chat(buddy, text):
+    newMessage('You: ' + text)
     buddy_list[buddy].send(('C;' + text).encode('utf-8'))
     print("message sent...\n\n")
     
 
 def broadcast(text):
+    newMessage('You' + ': ' + text)
     for buddy in buddy_list:
         buddy_list[buddy].send(('G;' + text).encode('utf-8'))
     print("message sent...\n\n")
@@ -161,21 +181,138 @@ def disconnect():
         #buddy_list[buddy].shutdown(socket.SHUT_RDWR)
         buddy_list[buddy].close()
     client_listener_stop.set()
+    newMessage('You have disconnected.')
     
 
-discover('127.0.0.1')
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
 
-input_thread = threading.Thread(target = input_processing, args = ())
-listener_thread = threading.Thread(target = port_listener, args = ())
+def connectdialog(root):
+    filewin = Toplevel(root)
+    row1 = Frame(filewin)
+    row1.pack(side=TOP, fill=X, padx=5, pady=5)
+    row2 = Frame(filewin)
+    row2.pack(side=TOP, fill=X, padx=5, pady=5)
+    row3 = Frame(filewin)
+    row3.pack(side=TOP, fill=X, padx=5, pady=5)
+    Label(row1, text="Peer address:", width=15, anchor='w').pack(side=LEFT)
+    addrentry = Entry(row1)
+    nameentry = Entry(row2)
+    #addrentry.bind("<Return>", (lambda e1=filewin, e2=addrentry.get(), e3=nameentry.get(): startconnecting(e1, e2, e3)))
+    #nameentry.bind("<Return>", (lambda e1=filewin, e2=addrentry.get(), e3=nameentry.get(): startconnecting(e1, e2, e3)))
+    addrentry.pack(side=RIGHT, expand=YES, fill=X)
+    nameentry.pack(side=RIGHT, expand=YES, fill=X)
+    Label(row2, text="Nickname:", width=15, anchor='w').pack(side=LEFT)
+    okbutton = Button(row3, text="Ok", command=lambda: startconnecting(filewin, addrentry.get(), nameentry.get()))
+    okbutton.pack(side=LEFT)
+    cancelbutton = Button(row3, text="Cancel", command=filewin.destroy)
+    cancelbutton.pack(side=RIGHT)
 
-listener_stop= threading.Event()
-client_listener_stop= threading.Event()
-input_stop= threading.Event()
+def startconnecting(root, addr, name):
+    global nickname
+    nickname = name
+    discover(addr)
 
-input_thread.start()
-listener_thread.start()
+    #input_thread = threading.Thread(target = input_processing, args = ())
+    listener_thread = threading.Thread(target = port_listener, args = ())
+    
+    #input_thread.start()
+    listener_thread.start()
+    root.destroy()
+    
+    #listener_thread.join()
+    #input_thread.join()
+    
+    #print("bye")
 
-listener_thread.join()
-input_thread.join()
 
-print("bye")
+def menubar(root):
+    menubar = Menu(root)
+    
+    connectionsmenu = Menu(menubar, tearoff=0)
+    connectionsmenu.add_command(label="Connect", command=lambda: connectdialog(root))
+    connectionsmenu.add_command(label="Disconnect", command=disconnect)
+    connectionsmenu.add_separator()
+    connectionsmenu.add_command(label="Exit", command=root.quit)
+    
+    menubar.add_cascade(label="Connections", menu=connectionsmenu)
+    
+    contactsmenu = Menu(menubar, tearoff=0)
+    contactsmenu.add_command(label="Manage contacts", command=donothing)
+    contactsmenu.add_command(label="Manage favorites", command=donothing)
+    
+    menubar.add_cascade(label="Contacts", menu=contactsmenu)
+    
+    settingsmenu = Menu(menubar, tearoff=0)
+    settingsmenu.add_command(label="Sound", command=donothing)
+    settingsmenu.add_command(label="Identity", command=donothing)
+    settingsmenu.add_command(label="Hotkeys", command=donothing)
+    settingsmenu.add_separator()
+    settingsmenu.add_command(label="Preferences", command=donothing)
+    
+    menubar.add_cascade(label="Settings", menu=settingsmenu)
+    
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="Help", command=donothing)
+    helpmenu.add_command(label="About...", command=donothing)
+    
+    menubar.add_cascade(label="Help", menu=helpmenu)
+    
+    root.config(menu=menubar)
+
+def chatbox(root):
+    scrollbar = Scrollbar(root)
+    scrollbar.pack( side = RIGHT, fill=Y )
+    
+    global chat_box
+    chat_box = Listbox(root, yscrollcommand = scrollbar.set, width=60)
+    #chat_box.insert(END, "You have connected.")
+    chat_box.pack( side = TOP, fill = BOTH )
+    #mylist.place(x=10, y=200)
+    scrollbar.config( command = chat_box.yview )
+
+def onlineframe(root):
+    scrollbar = Scrollbar(root)
+    scrollbar.pack( side = RIGHT, fill=Y )
+    #scrollbar.place
+    global gui_buddy_list
+    gui_buddy_list = Listbox(root, yscrollcommand = scrollbar.set, height=20, width=40)
+
+    gui_buddy_list.pack( side = TOP, fill = BOTH )
+    #mylist.place(x=10, y=10)
+    scrollbar.config( command = gui_buddy_list.yview )
+
+def update():
+    print(str(gui_buddy_list))
+    gui_buddy_list.delete(0, END)
+    for buddy in buddy_list:
+        gui_buddy_list.insert(END, buddy)
+
+def newMessage(msg):
+    chat_box.insert(END, msg)
+
+def gui():
+    root = Tk()
+    root.title('Tumbleweed')
+    root.minsize(507, 507)
+    
+    menubar(root)
+    
+    onlineframe(root)
+    
+    chatbox(root)
+    
+    text1 = Entry(root, bd=5)
+    text1.pack(side=BOTTOM)
+    text1.bind("<Return>", (lambda event, e=text1.get(): broadcast(e)))
+    button = Button(root, text="Send", command=lambda: broadcast(text1.get()))
+    button.pack(side=BOTTOM)
+    
+    root.mainloop()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
